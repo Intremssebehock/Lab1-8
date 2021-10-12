f1ecfe315921622585bb34ed1d9f37800e06b41d
38071 <38071@DESKTOP-8AVH0F5>
2021-10-12 17:17:05 +0300

    Добавьте файлы проекта.

---
+++ Lab1№8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab1_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x: ");
+            double x = double.Parse(Console.ReadLine());
+            double y = Convert.ToDouble((((3 * x - 5) * x + 2) * x - 1) * x + 7);
+            Console.WriteLine("Ответ: "+ y);
+
+            
+            
+        }
+    }
+}
